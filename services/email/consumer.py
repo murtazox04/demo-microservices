e05ed95fad5f7d3d58759664e6b5ae9cd0d49e78
murtazoxurramov <murtazoxurramov1@gmail.com>
2024-03-14 00:25:51 +0500

    Sending Emails in Email Service

--- services/email/consumer.py
+++ services/email/consumer.py
@@ -1,6 +1,13 @@
+import os
+import json
+import django
 from confluent_kafka import Consumer
+from django.core.mail import send_mail
 
 from decouple import config
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 consumer = Consumer({
     'bootstrap.servers': config("KAFKA_BOOTSTRAP_SERVERS"),
@@ -24,3 +31,23 @@ while True:
         continue
 
     print("Received message: {}".format(msg.value()))
+
+    order = json.loads(msg.value())
+
+    send_mail(
+        subject="An Order has been completed",
+        message="Order #" + str(order['id']) + "with a total of $" +
+        str(order['admin_revenue']) + " has been completed",
+        from_email="from@email.com",
+        recipient_list=["admin@admin.com"]
+    )
+
+    send_mail(
+        subject="An Order has been completed",
+        message="You earned $" +
+        str(order['ambassador_revenue']) + " from the link #" + order.code,
+        from_email="from@email.com",
+        recipient_list=[order['ambassador_email']]
+    )
+
+consumer.close()
